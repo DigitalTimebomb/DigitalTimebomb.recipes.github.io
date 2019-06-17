7b8deef68492bb19b9dfeeb70738062ad0153d43
Thomas Beaudry <tbeaudry78@gmail.com>
2019-06-17 09:26:56 -0400

    Add files via upload

---
+++ JS/External.js
@@ -0,0 +1,26 @@
+
+function MyTime() {
+  
+  document.getElementById("datetime").innerHTML;
+  var dt = new Date();
+
+}
+
+//function Hello(){
+  //  var VisitorName = prompt( "Hey man!, What is your name?");
+   // document.write("<h1>" + "Welcome " + VisitorName + "!" + "</h1>"); 
+//}
+
+const title = "'Best Reads of 2019'";
+const author = 'Thomas';
+let likes = 40;
+
+// Template Strings using the back tics ``
+let result = `The blog called ${title} by ${author} has ${likes} likes.`;
+console.log(result);
+
+let date = new Date();
+console.log(date);
+
+
+
